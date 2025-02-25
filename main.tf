4b1783d2114d8e08ce507ae4bc7ec909162e8774
Ray-tm <82286225+Ray-tm@users.noreply.github.com>
2025-02-25 16:19:57 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+data "github_user" "current" {
+  username = "Ray-tm"
+}
+
+resource "github_repository" "my_repo" {
+  name        = "envCreation"
+  description = "My code repository"
+}
+
+resource "github_repository_environment" "example" {
+  environment         = "dev"
+  repository          = github_repository.example.name
+  prevent_self_review = true
+  reviewers {
+    users = [data.github_user.current.id]
+  }
+  deployment_branch_policy {
+    protected_branches     = true
+    custom_branch_policies = false
+  }
+}
