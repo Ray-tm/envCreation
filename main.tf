bf5c43f6346e8fa17ad4c4c644e1a51a42a7526d
Ramiah Mashayamombe <ramiah.mashayamombe@jdplc.com>
2025-02-26 12:44:30 +0000

    changed provider.tf

--- main.tf
+++ main.tf
@@ -9,20 +9,20 @@ data "github_user" "current" {
 
 resource "github_repository_environment" "dev" {
   environment         = "dev"
-  repository          = var.my_repo
-  prevent_self_review = true
-  deployment_branch_policy {
-    protected_branches     = true
-    custom_branch_policies = false
-  }
+  repository          = "Ray-tm/envCreation"
+#   prevent_self_review = true
+#   deployment_branch_policy {
+#     protected_branches     = true
+#     custom_branch_policies = false
+#   }
 }
 
 resource "github_repository_environment" "pprod" {
   environment         = "pprod"
-  repository          = var.my_repo
-  prevent_self_review = true
-  deployment_branch_policy {
-    protected_branches     = true
-    custom_branch_policies = false
-  }
+  repository          = "Ray-tm/envCreation"
+#   prevent_self_review = true
+#   deployment_branch_policy {
+#     protected_branches     = true
+#     custom_branch_policies = false
+#   }
 }
